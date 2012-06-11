c0ea574520722c120b81cad7b05cb78803c04e06
Mark S <mscoolnerd@gmail.com>
2012-06-10 20:16:27 -0700

    added a basic service

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-palm-package -o ./build .
+
+# build with service
+#palm-package -o ./build app service package
+# or not
+palm-package -o ./build app package
 palm-install  build/com.invalidopcode.boxcar_1.0.0_all.ipk
 palm-launch -i com.invalidopcode.boxcar
--- depends.js
+++
@@ -1,6 +0,0 @@
-﻿enyo.depends(
-	"source/notificationListener.js",
-	"source/AppLauncher.js",
-	"source/jsHelpers.js",
-        "source/source/DashboardManager.js"
-);
